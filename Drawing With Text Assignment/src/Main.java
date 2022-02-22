b2ac9f93c61d61bb747c8872a5c1722a9ad8d069
Aaron Porter <aaronjtporter@icloud.com>
2022-02-22 15:08:25 -0800

    Drawing with Text Assignment Hand In

--- Drawing With Text Assignment/src/Main.java
+++ Drawing With Text Assignment/src/Main.java
@@ -2,6 +2,18 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        // Stick man made from "_" and "|" symbols
+
+        // I have separated the print lines into three parts
+        // (head, body and arms, legs) to make it easier to read in script
+
+        // print head
+        System.out.print("_________\n|\t\t|\n|\t\t|\n|_______|\n");
+
+        // print body and arms
+        System.out.print("\t|\n____|____\n|   |   |\n|   |   |\n|   |   |\n|   |   |\n\t|\n\t|\n\t|\n\t|\n\t|\n");
+
+        // print legs
+        System.out.print("   / \\\n  /   \\\n /     \\\n/       \\\n");
     }
 }
